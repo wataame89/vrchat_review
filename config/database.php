748a22d3bae8c532dbefd45cc5fc16a2770132ad
wataame89 <wataame200189@yahoo.co.jp>
2024-10-31 21:21:57 +0900

    Redisの設定

--- config/database.php
+++ config/database.php
@@ -142,6 +142,11 @@ return [
             'password' => env('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', 6379),
             'database' => env('REDIS_CACHE_DB', 1),
+            'options' => [
+                'ssl' => [
+                    'verify_peer' => false,
+                ],
+            ],
         ],
 
     ],
